51f48e493e65f9df087b7daf65b37f2e8a7d9cc1
limw <limw@hztywl.cn>
2021-11-08 00:08:27 +0800

    feat: week09

---
+++ week09:2021-11-01~2021-11-07/egsture.js
@@ -0,0 +1,38 @@
+const element = document.documentElement;
+
+element.addEventListener('mousedown', event => {
+
+})
+
+element.addEventListener('touchstart', event => {
+
+})
+
+element.addEventListener('touchmove', event => {
+
+})
+
+element.addEventListener('touchend', event => {
+
+})
+
+element.addEventListener('touchcancel', event => {
+
+})
+
+
+let start = point => {
+
+}
+
+let move = point => {
+
+}
+
+let end = point => {
+
+}
+
+let cancel = point => {
+    
+}
